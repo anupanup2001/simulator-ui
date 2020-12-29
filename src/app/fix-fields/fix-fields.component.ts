765633603d53d7d6a1c170844b6be1706c0c0cb1
Anup Arakkal <anup.anup2001@gmail.com>
2020-12-28 19:03:14 -0500

    Fix Fields Table sticky header

--- src/app/fix-fields/fix-fields.component.ts
+++ src/app/fix-fields/fix-fields.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FieldValue} from './field-value';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-fix-fields',
@@ -9,6 +10,7 @@ import {FieldValue} from './field-value';
 export class FixFieldsComponent implements OnInit {
 
   @Input() message: FieldValue[];
+  bogusDataSource = new MatTableDataSource<FieldValue>(null);
   displayedColumns: string[] = ['field', 'value'];
   constructor() { }
 
